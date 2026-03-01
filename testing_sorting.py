89f806a25200cb44e330838be27a2146aed15194
Pablo Guilló <pabloguilloj@gmail.com>
2026-03-01 18:09:18 +0000

    Implenting tests for insertion algorithm

---
+++ testing_sorting.py
@@ -0,0 +1,13 @@
+import unittest 
+import sorting
+class test_sorting(unittest.TestCase):
+
+    def test_insertion(self):
+        self.assertEqual(sorting.insertion([8,4,3,2,6,7,5,1]), [1,2,3,4,5,6,7,8])
+        self.assertEqual(sorting.insertion([1,2,3,4,5,6,8,7]), [1,2,3,4,5,6,7,8])
+        self.assertEqual(sorting.insertion([2,1]), [1,2])
+        self.assertEqual(sorting.insertion([]),[])
+
+if __name__ == "__main__":
+
+    unittest.main()
