57c09e5436728b8129c047ec4665222a8cdfc273
Pablo Guilló <pabloguilloj@gmail.com>
2026-03-01 22:16:33 +0000

    Implementing tests for selection, merge and heap sorting algorithms

--- testing_sorting.py
+++ testing_sorting.py
@@ -14,6 +14,24 @@ class test_sorting(unittest.TestCase):
         self.assertEqual(sorting.bubble([2,1]), [1,2])
         self.assertEqual(sorting.bubble([]),[])
 
+    def test_selection(self):
+        self.assertEqual(sorting.selection([8,4,3,2,6,7,5,1]), [1,2,3,4,5,6,7,8])
+        self.assertEqual(sorting.selection([1,2,3,4,5,6,8,7]), [1,2,3,4,5,6,7,8])
+        self.assertEqual(sorting.selection([2,1]), [1,2])
+        self.assertEqual(sorting.selection([]),[])
+
+    def test_merge(self):
+        self.assertEqual(sorting.merge([8,4,3,2,6,7,5,1]), [1,2,3,4,5,6,7,8])
+        self.assertEqual(sorting.merge([1,2,3,4,5,6,8,7]), [1,2,3,4,5,6,7,8])
+        self.assertEqual(sorting.merge([2,1]), [1,2])
+        self.assertEqual(sorting.merge([]),[])
+
+    def test_heap(self):
+        self.assertEqual(sorting.heap([8,4,3,2,6,7,5,1]), [1,2,3,4,5,6,7,8])
+        self.assertEqual(sorting.heap([1,2,3,4,5,6,8,7]), [1,2,3,4,5,6,7,8])
+        self.assertEqual(sorting.heap([2,1]), [1,2])
+        self.assertEqual(sorting.heap([]),[])
+        
 if __name__ == "__main__":
 
     unittest.main()
