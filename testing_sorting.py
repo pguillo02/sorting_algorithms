207d70fe123077dfb2bf79aefbc3357baf778f62
Pablo Guilló <pabloguilloj@gmail.com>
2026-03-01 18:15:01 +0000

    Implementing tests for bubble sort algorithm

--- testing_sorting.py
+++ testing_sorting.py
@@ -8,6 +8,12 @@ class test_sorting(unittest.TestCase):
         self.assertEqual(sorting.insertion([2,1]), [1,2])
         self.assertEqual(sorting.insertion([]),[])
 
+    def test_bubble(self):
+        self.assertEqual(sorting.bubble([8,4,3,2,6,7,5,1]), [1,2,3,4,5,6,7,8])
+        self.assertEqual(sorting.bubble([1,2,3,4,5,6,8,7]), [1,2,3,4,5,6,7,8])
+        self.assertEqual(sorting.bubble([2,1]), [1,2])
+        self.assertEqual(sorting.bubble([]),[])
+
 if __name__ == "__main__":
 
     unittest.main()
